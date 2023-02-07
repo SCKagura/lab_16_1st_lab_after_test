46500064234bc1716bc898fefba8973c460dc7c7
= <=>
2023-02-07 15:56:00 +0700

    lab16_1_completed

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -1,9 +1,29 @@
-include <iostream>
+#include <iostream>
+#include<string>
 using namespace std;
 
 int main(){
-
-	
-	
+	int a = 5 ;
+	string b = "A" ;
+	string &c = b ; 
+	int *x = &a ;
+	string *y = &b ;
+	int **z = &x ;
+	cout << a << " " << b <<" "<<c << " " << x << " " << y << " " << z << " " ; 
+	cout << endl ;
+	cout << &a << " " << &b << " " << &c << " "  << &x << " " << &y << " " << &z << " " ;
+	cout << endl ;
+	c = "F" ;
+	cout << a << " " << b <<" "<<c << " " << x << " " << y << " " << z << " " ; 
+	cout << endl ;
+	*y  = "W" ;
+	cout << a << " " << b <<" "<<c << " " << x << " " << y << " " << z << " " ; 
+	cout << endl ;
+	*x = 6 ;
+	cout << a << " " << b <<" "<<c << " " << x << " " << y << " " << z << " " ; 
+	cout << endl ;
+	**z = 7  ;
+	cout << a << " " << b <<" "<<c << " " << x << " " << y << " " << z << " " ; 
+	cout << endl ;
 	return 0;
 }
